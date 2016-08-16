9b37ef6d4bff78af06dd8ccb4a42c74aabe68325
Gilvan Almeida <g6almeida@hotmail.com>
2016-08-16 00:45:04 -0300

    redirecionamento após edição para a párina principal.

--- tarefas/editar.php
+++ tarefas/editar.php
@@ -33,6 +33,8 @@ if (isset($_GET['nome']) && $_GET['nome'] !=''){
     }
 
     editar_tarefa($conexao, $tarefa);
+    header('Location: tarefas.php');
+    die();
 }
 
 $tarefa = buscar_tarefa($conexao, $_GET['id']);
