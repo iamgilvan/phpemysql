eec348d3ca49b73ca34ba3a8c53eb8f6247546cc
Gilvan Almeida <g6almeida@hotmail.com>
2016-08-16 22:12:13 -0300

    Certificar de que as datas vieram com os três campos, e validando as datas possíveis.

--- tarefas/ajudantes.php
+++ tarefas/ajudantes.php
@@ -6,6 +6,10 @@ function traduz_data_para_banco($data){
     }
 
     $dados = explode("/", $data);
+
+    if (count($dados) != 3){
+        return $data;
+    }
     $data_mysql = "{$dados[2]}-{$dados[1]}-{$dados[0]}";
     return $data_mysql;
 }
@@ -16,6 +20,10 @@ function traduz_data_para_exibir($data){
     }
 
     $dados = explode("-", $data);
+
+    if (count($dados) != 3){
+        return $data;
+    }
 
     $data_exibir = "{$dados[2]}/{$dados[1]}/{$dados[0]}";
     return $data_exibir;
@@ -55,5 +63,18 @@ function tem_post(){
 function validar_data($data){
     $padrao = '/^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/';
     $resultado = preg_match($padrao, $data);
+
+    if (! $resultado){
+        return false;
+    }
+
+    $dados = explode("/", $data);
+
+    $dia = $dados[0];
+    $mes = $dados[1];
+    $ano = $dados[2];
+
+    $resultado = checkdate($mes, $dia, $ano);
+
     return $resultado;
 }
