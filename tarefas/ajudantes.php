0f3c2216fa57924f530ed6112a11e21eeb8cbc7c
Gilvan Almeida <g6almeida@hotmail.com>
2016-08-24 18:09:19 -0300

    Recebendo arquivos zip ou pdf

--- tarefas/ajudantes.php
+++ tarefas/ajudantes.php
@@ -78,3 +78,15 @@ function validar_data($data){
 
     return $resultado;
 }
+
+function tratar_anexo($anexo){
+    $padrao = '/^.+(\.pdf|\.zip)$/';
+    $resultado = preg_match($padrao, $anexo['name']);
+
+    if (! $resultado){
+        return false;
+    }
+
+    move_uploaded_file($anexo['tmp_name'],"anexos/{$anexo['name']}");
+    return true;
+}
