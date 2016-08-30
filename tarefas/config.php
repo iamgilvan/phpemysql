21d6696d494c0a23574cb050b6b9c00d73d3219d
Gilvan Almeida <g6almeida@hotmail.com>
2016-08-30 19:50:04 -0300

    Definindo constantes.

---
+++ tarefas/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Conexão ao banco de dados (MySQL)
+define("BD_SERVIDOR", "127.0.0.1");
+define("BD_USUARIO", "sistematarefa");
+define("BD_SENHA", "sistema");
+define("BD_BANCO", "tarefa");
+
+//E-mail para notificação
+define("EMAIL_NOTIFICACAO","g6almeida@hotmail.com");
